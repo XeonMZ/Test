8469b70b76756bdc045de61ccc24f116b4a66ba9
LyTe DEV <100498333+XeonMZ@users.noreply.github.com>
2026-07-12 11:15:38 +0700

    Delete backend directory

--- backend/app/Support/FeatureFlags/FeatureFlagRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Support\FeatureFlags;
-
-interface FeatureFlagRepository
-{
-    public function enabled(FeatureFlagDefinition $flag): bool;
-    public function set(FeatureFlagDefinition $flag, bool $enabled): void;
-}
