97e0f476de5b04ba2caaebca29230afd24f9ade2
LyTe DEV <100498333+XeonMZ@users.noreply.github.com>
2026-07-20 21:57:50 +0700

    Delete backend directory

--- backend/app/Support/Enums/UserRole.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace App\Support\Enums;
-
-enum UserRole: string { case Guest = 'guest'; case Customer = 'customer'; case Driver = 'driver'; case Admin = 'admin'; case Owner = 'owner'; }
