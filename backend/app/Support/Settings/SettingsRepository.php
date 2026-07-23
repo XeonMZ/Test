216601052a3116bdebc1d4bc79094c23988cbd69
LyTe DEV <100498333+XeonMZ@users.noreply.github.com>
2026-07-23 13:44:50 +0700

    Delete backend directory

--- backend/app/Support/Settings/SettingsRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Support\Settings;
-
-interface SettingsRepository
-{
-    public function get(SettingKey $key): mixed;
-    public function put(SettingKey $key, mixed $value): void;
-}
